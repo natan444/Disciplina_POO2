0af8fc36d72976948125e4b4e3241a9ec9a25413
Natan Araujo <natanmendesaraujo13@outlook.com>
2025-03-28 21:17:51 -0300

    versao inicial do projeto

---
+++ src/arqdeoos1a2025/ARQDEOOS1A2025.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arqdeoos1a2025;
+
+/**
+ *
+ * @author aluno
+ */
+public class ARQDEOOS1A2025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+    
+}
